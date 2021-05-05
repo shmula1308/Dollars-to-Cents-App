2600fb8d43228ac214922534b7d61f713c2cae83
Shpend Mula <shmula@hotmail.com>
2021-05-05 17:31:15 +0200

    updated css

---
+++ js/converter.js
@@ -0,0 +1,43 @@
+// limit input to no more than 8 digits
+//let dollars = 353.34;
+// let cents = dollars * 100; //get cents
+// let quarters = Math.floor(cents / 25); //get quarters
+// let remainder = cents%25; //get reaminder after getting quarters
+// let dimes,temp,nickles,pennies
+// console.log('remainder',remainder)
+
+// if(remainder >= 10) {
+//   dimes = Math.floor(remainder/10);
+//   temp = remainder % 10;
+//   console.log('temp',temp,dimes)
+// }
+
+// if(temp >= 5) {
+//   nickels = Math.floor(temp / 5);
+//   pennies = temp % 5;
+// }
+
+// if(temp < 5 || temp === 0) {
+//   nickels = 0;
+//   pennies = temp;
+  
+// }
+
+// if(remainder >= 5 && remainder < 10) {
+//   nickels = Math.floor(remainder / 5);
+//   pennies = remainder % 5;
+//   dimes = 0;
+// }
+
+// if(remainder < 5) {
+//   pennies = remainder;
+//   dimes = 0;
+//   nickels = 0;
+// }
+
+
+
+
+
+// console.log(cents,quarters,dimes,nickels,pennies)
+
